3c6154b5e0de909822d13ff70aea257e018fb191
Antoine <37471522+antoinesolovyov@users.noreply.github.com>
2021-12-02 00:16:16 +0300

    Update api.js

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,8 +1,8 @@
 export const WEATHER_API = 'https://api.openweathermap.org/data/2.5/'
-export const WEATHER_API_KEY = '7cd1581792cb7fa3710cd45312c6415e'
+export const WEATHER_API_KEY = ''
 
 export const GEOCODE_API = 'https://maps.googleapis.com/maps/api/geocode/'
-export const GEOCOE_API_KEY = 'AIzaSyDt-koV9hJiZZBL_CguOs81KnGpwPjZo3c'
+export const GEOCOE_API_KEY = ''
 
 export const getIconPath = (day) => `./condition-icons/${day?.main.toLowerCase()}.svg`
 export const getWeatherUrl = (lat, lon) => `${WEATHER_API}onecall?lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_API_KEY}`
